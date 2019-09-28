18f972da039c5b26d6b6057ec04c18f163e0f7a0
Mika Berglund <mika.berglund@meteoriitti.com>
2019-09-29 01:37:27 +0300

    Added class name constants.

--- BlazorBootstrap.Components/ClassNames.cs
+++ BlazorBootstrap.Components/ClassNames.cs
@@ -6,6 +6,119 @@ namespace BlazorBootstrap.Components
 {
     public static class ClassNames
     {
+
+        public static class Abbreviations
+        {
+            /// <summary>
+            /// Gives a slightly smaller font size to an abbreviation.
+            /// </summary>
+            public const string Initialism = "initialism";
+
+        }
+
+        public static class Alignments
+        {
+            /// <summary>
+            /// Centers the text.
+            /// </summary>
+            public const string TextCenter = "text-center";
+
+            /// <summary>
+            /// Aligns text to the right.
+            /// </summary>
+            public const string TextRight = "text-right";
+        }
+
+        public class BlockQuotes
+        {
+            /// <summary>
+            /// A blockquote element.
+            /// </summary>
+            public const string Blockquote = "blockquote";
+
+            /// <summary>
+            /// A footer element inside of a blockquote element.
+            /// </summary>
+            public const string Footer = "blockquote-footer";
+        }
+
+        public static class Headings
+        {
+            /// <summary>
+            /// Use on header to make it bigger than the default size. Display 1 is the biggest size.
+            /// </summary>
+            public const string Display1 = "display-1";
+
+            /// <summary>
+            /// Use on header to make it bigger than the default size. Display 2 is the second biggest size.
+            /// </summary>
+            public const string Display2 = "display-2";
+
+            /// <summary>
+            /// Use on header to make it bigger than the default size. Display 3 is the third biggest size.
+            /// </summary>
+            public const string Display3 = "display-3";
+
+            /// <summary>
+            /// Use on header to make it bigger than the default size. Display 4 is the fourth biggest size.
+            /// </summary>
+            public const string Display4 = "display-4";
+
+            /// <summary>
+            /// Heading 1
+            /// </summary>
+            public const string H1 = "h1";
+
+            /// <summary>
+            /// Heading 2
+            /// </summary>
+            public const string H2 = "h2";
+
+            /// <summary>
+            /// Heading 3
+            /// </summary>
+            public const string H3 = "h3";
+
+            /// <summary>
+            /// Heading 4
+            /// </summary>
+            public const string H4 = "h4";
+
+            /// <summary>
+            /// Heading 5
+            /// </summary>
+            public const string H5 = "h5";
+
+            /// <summary>
+            /// Heading 6
+            /// </summary>
+            public const string H6 = "h6";
+        }
+
+        public static class InlineText
+        {
+
+            /// <summary>
+            /// Applies the same styling as <c>&lt;mark /&gt;</c>.
+            /// </summary>
+            public const string Mark = "mark";
+
+            /// <summary>
+            /// Applies the same styling as <c>&lt;small /&gt;</c>.
+            /// </summary>
+            public const string Small = "small";
+
+            /// <summary>
+            /// Gives the text a faded look.
+            /// </summary>
+            public const string TextMuted = "text-muted";
+        }
+
+        public static class Lists
+        {
+
+        }
+
         public static class Paragraphs
         {
             /// <summary>
@@ -13,5 +126,6 @@ namespace BlazorBootstrap.Components
             /// </summary>
             public const string Lead = "lead";
         }
+
     }
 }
