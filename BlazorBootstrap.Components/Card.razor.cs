9952ba1b5bf31f46b628cc7d655b0c16ba1b5db6
Mika Berglund <mika.berglund@meteoriitti.com>
2019-11-07 15:07:33 +0200

    Fixed compilation errors due to link changes.

--- BlazorBootstrap.Components/Card.razor.cs
+++ BlazorBootstrap.Components/Card.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BlazorBootstrap.Components.Model;
+using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
