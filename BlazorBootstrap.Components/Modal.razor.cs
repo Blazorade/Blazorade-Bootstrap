a914cd7af0711862cd4047545301293adf02e180
Mika Berglund <mika.berglund@meteoriitti.com>
2019-12-24 10:10:09 +0200

    Added support for different sizes of Modal.

--- BlazorBootstrap.Components/Modal.razor.cs
+++ BlazorBootstrap.Components/Modal.razor.cs
@@ -10,11 +10,20 @@ namespace BlazorBootstrap.Components
     public partial class Modal
     {
 
+        public Modal()
+        {
+            this.Fade = true;
+            this.Size = ModalSize.Default;
+        }
+
         [Parameter]
         public string Body { get; set; }
 
         [Parameter]
         public RenderFragment<string> BodyTemplate { get; set; }
+
+        [Parameter]
+        public bool Fade { get; set; }
 
         [Parameter]
         public RenderFragment FooterTemplate { get; set; }
@@ -25,11 +34,18 @@ namespace BlazorBootstrap.Components
         [Parameter]
         public RenderFragment<string> HeaderTemplate { get; set; }
 
+        [Parameter]
+        public ModalSize Size { get; set; }
+
 
 
         [Inject]
         protected IJSRuntime JsInterop { get; set; }
 
+
+
+
+        private string DialogClasses { get; set; }
 
 
         /// <summary>
@@ -53,11 +69,25 @@ namespace BlazorBootstrap.Components
             this.SetIdIfEmpty();
 
             this.AddClass(ClassNames.Modals.Modal);
-            this.AddClass(ClassNames.Fade);
+            if (this.Fade)
+            {
+                this.AddClass(ClassNames.Fade);
+            }
 
             this.AddAttribute("role", "dialog");
             this.AddAttribute("tabindex", "-1");
             this.AddAttribute("aria-hidden", "true");
+
+            var dialogClasses = new List<string>
+            {
+                ClassNames.Modals.Dialog
+            };
+            if(this.Size != ModalSize.Default)
+            {
+                dialogClasses.Add($"{ClassNames.Modals.Modal}-{this.Size.ToString().ToLower()}");
+            }
+            this.DialogClasses = string.Join(" ", dialogClasses);
+
 
             base.OnParametersSet();
         }
---
+++ BlazorBootstrap.Components/ModalSize.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBootstrap.Components
+{
+    public enum ModalSize
+    {
+        Default,
+
+        /// <summary>
+        /// Small.
+        /// </summary>
+        SM,
+
+        /// <summary>
+        /// Large.
+        /// </summary>
+        LG,
+
+        /// <summary>
+        /// Extra large.
+        /// </summary>
+        XL
+    }
+}
