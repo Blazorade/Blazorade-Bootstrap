7432685ddd88904dd8b4be9465938b59b6e4236c
Mika Berglund <mika.berglund@meteoriitti.com>
2019-12-30 13:59:00 +0200

    Published a new version of the Showroom app.

---
+++ docs/_content/BlazorBootstrap.Components/bbs.js
@@ -0,0 +1,53 @@
+﻿
+
+window.blazorBootstrap = {
+    hide: function (elementId) {
+        $("#" + elementId).addClass("d-none");
+    },
+    show: function (elementId) {
+        $("#" + elementId).removeClass("d-none");
+    },
+    alerts: {
+        dismiss: function(selector) {
+            $(selector).alert("close");
+        }
+    },
+
+    anchors: {
+        init: function (elementId, anchorId) {
+            $("#" + anchorId).attr("onclick", "return false;");
+        },
+        scrollIntoView: function (elementId) {
+            var element = document.getElementById(elementId);
+            if (element) {
+                element.scrollIntoView();
+                window.location.hash = elementId;
+            }
+            else {
+                console.warn("Could not find element by id.", elementId);
+            }
+        }
+    },
+
+    carousels: {
+        carousel: function(selector) {
+            var c = $(selector);
+            var result = c.carousel();
+
+            var id = c.attr("id");
+            c.find(".carousel-control").attr("data-target", "#" + id);
+        }
+    },
+
+    modals: {
+        hide: function (selector) {
+            $(selector).modal("hide");
+        },
+        show: function (selector) {
+            $(selector).modal("show");
+        },
+        toggle: function (selector) {
+            $(selector).modal("toggle");
+        }
+    }
+}
