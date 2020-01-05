e35438cab5c23a7dbc428cb166670ee1b13797d8
Mika Berglund <mika.berglund@meteoriitti.com>
2020-01-05 11:06:45 +0200

    Added a very simple version of Nav, and added it to the Showroom application.

---
+++ BlazorBootstrap.Components/NavLink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBootstrap.Components
+{
+    public class NavLink : Anchor
+    {
+        protected override void OnParametersSet()
+        {
+            this.AddClass(ClassNames.Navs.Link);
+            base.OnParametersSet();
+        }
+    }
+}
