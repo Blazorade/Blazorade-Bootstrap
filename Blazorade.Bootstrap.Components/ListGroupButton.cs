2e7aaa72900ce95eb02481aa86184fb221696be8
EvanWeeks <eweeks@colpipe.com>
2020-02-05 14:53:54 -0500

    Added ListGroupItem, ListGroupAnchor and ListGroupButton, as well as some demo code in the showroom. Added the flush and horizontal classes to the ListGroups classes in ClassNames.cs.

---
+++ Blazorade.Bootstrap.Components/ListGroupButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazorade.Bootstrap.Components
+{
+    public class ListGroupButton : Button
+    {
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            this.AddClasses(ClassNames.ListGroups.Item, ClassNames.ListGroups.Action);
+
+            // replace the button coloration with the list item coloration
+            this.RemoveClass(this.GetColorClassName(prefix: !this.IsOutline ? ClassNames.Buttons.Button : ClassNames.Buttons.OutlineButton, color: this.Color));
+            this.AddClasses(this.GetColorClassName(prefix: ClassNames.ListGroups.Item, this.Color));
+        }
+    }
+}
---
+++ Blazorade.Bootstrap.Components/ListGroupItem.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazorade.Bootstrap.Components
+{
+    public partial class ListGroupItem
+    {
+        [Parameter]
+        public bool IsAction { get; set; }
+
+        [Parameter]
+        public bool IsActive { get; set; }
+
+        [Parameter]
+        public bool IsDisabled { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            this.AddClasses(ClassNames.ListGroups.Item);
+
+            if(this.IsAction)
+            {
+                this.AddClasses(ClassNames.ListGroups.Action);
+            }
+
+            if(this.IsActive)
+            {
+                this.AddClasses(ClassNames.Active);
+            }
+
+            if(this.IsDisabled)
+            {
+                this.AddClasses(ClassNames.Disabled);
+            }
+
+            if(Color != null)
+            {
+                this.AddClasses(this.GetColorClassName(prefix: ClassNames.ListGroups.Item, this.Color));
+            }
+
+            base.OnParametersSet();
+        }
+    }
+}
