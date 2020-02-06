7ba792a2ba3c45baec9915ebe107103a6b679951
Evan Weeks <EWeeks@colpipe.com>
2020-02-06 10:02:06 -0500

    Add the IsAnimated property and example on the Showroom screen. Added the MainMenu link to the showroom screen.

--- Blazorade.Bootstrap.Components/ProgressBar.razor.cs
+++ Blazorade.Bootstrap.Components/ProgressBar.razor.cs
@@ -7,22 +7,40 @@ namespace Blazorade.Bootstrap.Components
 {
     public partial class ProgressBar
     {
+        /// <summary>
+        /// If set to true, this progress bar will have a striped texture.
+        /// </summary>
         [Parameter]
         public bool IsStriped { get; set; }
 
+        /// <summary>
+        /// If set to true, any stripe will be animated.
+        /// </summary>
+        [Parameter]
+        public bool IsAnimated { get; set; }
+
+        /// <summary>
+        /// The minimum value represented by the progress bar. This must be less than both Value and ValueMax.
+        /// </summary>
         [Parameter]
         public double ValueMin { get; set; }
 
+        /// <summary>
+        /// The maximum value represented by the progress bar. This must be greater than both ValueMin and Value.
+        /// </summary>
         [Parameter]
         public double ValueMax { get; set; }
 
+        /// <summary>
+        /// The current value represented by the progress bar. This must fall between ValueMin and ValueMax.
+        /// </summary>
         [Parameter]
         public double Value { get; set; }
 
-        public string CalculatedWidth {
+        protected string CalculatedWidth {
             get
             {
-                return $"{Math.Round(Value / ValueMax)}%;";
+                return $"{Math.Round(Value / ValueMax, 2) * 100}%;";
             }
         }
 
@@ -36,29 +54,25 @@ namespace Blazorade.Bootstrap.Components
                 this.AddClasses(ClassNames.ProgressBars.Striped);
             }
 
-            if(null != this.Color)
+            if (this.IsAnimated)
+            {
+                this.AddClasses(ClassNames.ProgressBars.Animated);
+            }
+
+            if (null != this.Color)
             {
                 this.AddClasses(this.GetColorClassName("bg", this.Color));
             }
 
-            //// check and throw if contraints are busted
-            //if(ValueMax < ValueMin)
-            //{
-            //    throw new ArgumentOutOfRangeException("ValueMax must be greater than ValueMin");
-            //}
-            //else if(Value > ValueMax || Value < ValueMin)
-            //{
-            //    throw new ArgumentOutOfRangeException("Value must be greater than ValueMin and less than ValueMax");
-            //}
-            //else
-            //{
-            //    // replace the "width" style on this element
-            //    if(this.Styles.ContainsKey("width"))
-            //    {
-            //        this.RemoveStyle("width");
-            //    }
-            //    this.AddStyle("width", $"{Math.Round(Value / ValueMax)}%");
-            //}
+            // check and throw if contraints are busted
+            if (ValueMax < ValueMin)
+            {
+                throw new ArgumentOutOfRangeException("ValueMax must be greater than ValueMin");
+            }
+            else if (Value > ValueMax || Value < ValueMin)
+            {
+                throw new ArgumentOutOfRangeException("Value must be greater than ValueMin and less than ValueMax");
+            }
 
             base.OnParametersSet();
         }
