e695f4ec6330d6aff0be71360e7bda0c287eac80
Evan Weeks <EWeeks@colpipe.com>
2020-02-06 08:20:54 -0500

    Changed the <Div> to a <div> in order to test using bound attributes inside of the component. Also added the Showroom page for ProgressBars.

--- Blazorade.Bootstrap.Components/ProgressBar.razor.cs
+++ Blazorade.Bootstrap.Components/ProgressBar.razor.cs
@@ -19,6 +19,13 @@ namespace Blazorade.Bootstrap.Components
         [Parameter]
         public double Value { get; set; }
 
+        public string CalculatedWidth {
+            get
+            {
+                return $"{Math.Round(Value / ValueMax)}%;";
+            }
+        }
+
         protected override void OnParametersSet()
         {
             this.AddClasses(ClassNames.ProgressBars.ProgressBar);
@@ -34,24 +41,24 @@ namespace Blazorade.Bootstrap.Components
                 this.AddClasses(this.GetColorClassName("bg", this.Color));
             }
 
-            // check and throw if contraints are busted
-            if(ValueMax < ValueMin)
-            {
-                throw new ArgumentOutOfRangeException("ValueMax must be greater than ValueMin");
-            }
-            else if(Value > ValueMax || Value < ValueMin)
-            {
-                throw new ArgumentOutOfRangeException("Value must be greater than ValueMin and less than ValueMax");
-            }
-            else
-            {
-                // replace the "width" style on this element
-                if(this.Styles.ContainsKey("width"))
-                {
-                    this.RemoveStyle("width");
-                }
-                this.AddStyle("width", $"{Math.Round(Value / ValueMax)}%");
-            }
+            //// check and throw if contraints are busted
+            //if(ValueMax < ValueMin)
+            //{
+            //    throw new ArgumentOutOfRangeException("ValueMax must be greater than ValueMin");
+            //}
+            //else if(Value > ValueMax || Value < ValueMin)
+            //{
+            //    throw new ArgumentOutOfRangeException("Value must be greater than ValueMin and less than ValueMax");
+            //}
+            //else
+            //{
+            //    // replace the "width" style on this element
+            //    if(this.Styles.ContainsKey("width"))
+            //    {
+            //        this.RemoveStyle("width");
+            //    }
+            //    this.AddStyle("width", $"{Math.Round(Value / ValueMax)}%");
+            //}
 
             base.OnParametersSet();
         }
