2d36204bee386bf56dbc9dfc774bd7c4c15618c5
Evan Weeks <evan.weeks@gmail.com>
2020-02-07 23:02:01 -0500

    Added a code-behind for PaginationLink SOLELY to add some XML docs for Intellisense. Changed the showroom example to bind to the link instead of binding to the link's properties.

---
+++ Blazorade.Bootstrap.Components/PaginationLink.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazorade.Bootstrap.Components
+{
+    /// <summary>
+    /// The PaginationLink component represents the list item/anchor structure of a bootstrap "page-item" and "page-link".
+    ///  It must be used as a child element of a Pagination component, and can either make use of a bound <see cref="ILink"/>'s 
+    ///  "Text" property, a custom-defined "Text" property, or define its own child content.
+    /// </summary>
+    /// <inheritdoc cref="Anchor"/>
+    public partial class PaginationLink
+    {
+    }
+}
