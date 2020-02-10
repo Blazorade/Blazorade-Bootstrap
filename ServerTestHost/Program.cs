2dd8cb1c479311768f8983f3ec9215ceeaba9db4
Evan Weeks <EWeeks@colpipe.com>
2020-02-10 10:53:44 -0500

    Removed the .Net Core console logger.

--- ServerTestHost/Program.cs
+++ ServerTestHost/Program.cs
@@ -20,11 +20,6 @@ namespace ServerTestHost
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.AddConsole();
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
