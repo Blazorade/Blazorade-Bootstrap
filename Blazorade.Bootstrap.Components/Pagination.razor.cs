88c9edd3d64ffcaed150ccbcd53446159106d85d
Mika Berglund <mika.berglund@meteoriitti.com>
2020-02-10 11:18:00 +0200

    Simplifying pagination. WIP.

--- Blazorade.Bootstrap.Components/Pagination.razor.cs
+++ Blazorade.Bootstrap.Components/Pagination.razor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Blazorade.Bootstrap.Components.Model;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -12,6 +13,25 @@ namespace Blazorade.Bootstrap.Components
     /// </summary>
     public partial class Pagination
     {
+        public Pagination()
+        {
+            this.ShowNavigation = true;
+
+            this.NextPageLinkText = ">>";
+            this.PreviousPageLinkText = "<<";
+        }
+
+        /// <summary>
+        /// Fired when the <see cref="CurrentPage"/> parameter has changed.
+        /// </summary>
+        [Parameter]
+        public EventCallback<Pagination> OnCurrentPageChanged { get; set; }
+
+        /// <summary>
+        /// The zero-based index of the currently selected page.
+        /// </summary>
+        [Parameter]
+        public int CurrentPage { get; set; }
 
         /// <summary>
         /// Set the size of the pagination component. Defaults to "Normal"
@@ -20,11 +40,124 @@ namespace Blazorade.Bootstrap.Components
         public PaginationSize Size { get; set; } = PaginationSize.Normal;
 
         /// <summary>
-        /// A list of <see cref="ILink"/> objects to use as page links.
-        /// These MUST have the Url field populated to show up in the pagination!
+        /// The number of pages to show.
         /// </summary>
         [Parameter]
-        public IReadOnlyList<ILink> Items { get; set; }
+        public int PageCount { get; set; }
+
+        /// <summary>
+        /// Specifies whether to show the Next and Previous navigation controls.
+        /// </summary>
+        [Parameter]
+        public bool ShowNavigation { get; set; }
+
+        /// <summary>
+        /// The template that allows you to customize how the next page link is rendered.
+        /// </summary>
+        [Parameter]
+        public RenderFragment NextPageLinkTemplate { get; set; }
+
+        /// <summary>
+        /// The text to show in the next page link.
+        /// </summary>
+        [Parameter]
+        public string NextPageLinkText { get; set; }
+
+        /// <summary>
+        /// The template that allows you to customize how a page item link is rendered. The context parameter passed to
+        /// the template is the zero-based index of the page item to render.
+        /// </summary>
+        [Parameter]
+        public RenderFragment<int> PageItemLinkTemplate { get; set; }
+
+        /// <summary>
+        /// The template that allows you to customize how the previous page link is rendered.
+        /// </summary>
+        [Parameter]
+        public RenderFragment PreviousPageLinkTemplate { get; set; }
+
+        /// <summary>
+        /// The text to show in the previous page link.
+        /// </summary>
+        [Parameter]
+        public string PreviousPageLinkText { get; set; }
+
+
+        /// <summary>
+        /// Navigates to the given page.
+        /// </summary>
+        /// <param name="pageIndex">The zero-based index of the page to navigate to.</param>
+        public async Task NavigateToAsync(int pageIndex)
+        {
+            if(pageIndex >= 0 && pageIndex < this.PageCount)
+            {
+                if(this.CurrentPage != pageIndex)
+                {
+                    this.CurrentPage = pageIndex;
+                    await this.OnCurrentPageChangedAsync();
+                }
+            }
+            else
+            {
+                // Should never come here.
+            }
+        }
+
+        /// <summary>
+        /// Navigates to the next page.
+        /// </summary>
+        public async Task NextAsync()
+        {
+            if(this.CurrentPage < this.PageCount - 1)
+            {
+                this.CurrentPage++;
+                await this.OnCurrentPageChangedAsync();
+            }
+            else
+            {
+                // This should not occur with the default setup, so if it happens, the developer has tweeked the control so maybe we need to log something.
+
+            }
+        }
+
+        /// <summary>
+        /// Navigates to the previous page.
+        /// </summary>
+        public async Task PreviousAsync()
+        {
+            if(this.CurrentPage > 0)
+            {
+                this.CurrentPage--;
+                await this.OnCurrentPageChangedAsync();
+            }
+            else
+            {
+                // Again, this should not happen, so we might want to log something to the developer.
+            }
+        }
+
+
+
+        protected IEnumerable<int> CreatePageIndexArray()
+        {
+            int ix = 0;
+
+            while(ix < this.PageCount)
+            {
+                yield return ix;
+                ix++;
+            }
+
+            yield break;
+        }
+
+        /// <summary>
+        /// Fires the <see cref="OnCurrentPageChanged"/> event.
+        /// </summary>
+        protected virtual async Task OnCurrentPageChangedAsync()
+        {
+            await this.OnCurrentPageChanged.InvokeAsync(this);
+        }
 
 
         protected override void OnParametersSet()
@@ -67,4 +200,5 @@ namespace Blazorade.Bootstrap.Components
         /// </summary>
         Small
     }
+
 }
