f1055217df0699f59ce2351049d514c83d5c504a
Mika Berglund <mika.berglund@meteoriitti.com>
2020-02-28 15:58:04 +0200

    Added XML docs to CardImage. #42

--- Blazorade.Bootstrap.Components/CardImage.razor.cs
+++ Blazorade.Bootstrap.Components/CardImage.razor.cs
@@ -5,15 +5,23 @@ using System.Text;
 
 namespace Blazorade.Bootstrap.Components
 {
+    /// <summary>
+    /// Renders an image in a <see cref="Card"/> component.
+    /// </summary>
     public partial class CardImage
     {
 
-
+        /// <summary>
+        /// Specifies whether the card is an overlay image, i.e. the contents of the card are
+        /// overlaid ver the image.
+        /// </summary>
         [Parameter]
         public bool IsOverlay { get; set; }
 
 
 
+        /// <summary>
+        /// </summary>
         protected override void OnParametersSet()
         {
             if (!this.IsOverlay)
