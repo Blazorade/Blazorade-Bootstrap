c56ce4860eec4c8e34f6010bc8ab22061ecb4c1d
Mika Berglund <mika.berglund@meteoriitti.com>
2020-02-28 15:59:58 +0200

    Added XML docs to CardImagePosition. #43

--- Blazorade.Bootstrap.Components/CardImagePosition.cs
+++ Blazorade.Bootstrap.Components/CardImagePosition.cs
@@ -4,12 +4,34 @@ using System.Text;
 
 namespace Blazorade.Bootstrap.Components
 {
+    /// <summary>
+    /// Defines different positions for images contained in <see cref="Card"/> components.
+    /// </summary>
     public enum CardImagePosition
     {
+        /// <summary>
+        /// Default. The image is positioned at the top of the card.
+        /// </summary>
         Top,
+
+        /// <summary>
+        /// The image is positioned at the bottom of the card.
+        /// </summary>
         Bottom,
+
+        /// <summary>
+        /// The image is positioned to the left of the card.
+        /// </summary>
         Left,
+
+        /// <summary>
+        /// The image is positioned to the right of the card.
+        /// </summary>
         Right,
+
+        /// <summary>
+        /// The contents of the card are overlaid over the image.
+        /// </summary>
         Overlay
     }
 }
