ee9fc3ac87929ef00b926edfda295e5881f719f3
Mika Berglund <mika.berglund@meteoriitti.com>
2020-02-28 17:31:27 +0200

    Added XML docs to Navbar. Fixes #68

--- Blazorade.Bootstrap.Components/Navbar.razor.cs
+++ Blazorade.Bootstrap.Components/Navbar.razor.cs
@@ -6,39 +6,72 @@ using System.Text;
 
 namespace Blazorade.Bootstrap.Components
 {
+    /// <summary>
+    /// The Navbar component is used to produce a responsive navigation header with built-in support for collapsing.
+    /// </summary>
     public partial class Navbar
     {
+        /// <summary>
+        /// </summary>
         public Navbar()
         {
             this.ExpandAt = NavbarExpandBreakpoint.MD;
         }
 
 
+        /// <summary>
+        /// The text to use as the Brand text on the Navbar. The brand is the top-left element on the Navbar,
+        /// which typically displays the name of the site or application.
+        /// </summary>
         [Parameter]
         public string BrandText { get; set; }
 
+        /// <summary>
+        /// The link URL to use for the Brand text.
+        /// </summary>
         [Parameter]
         public string BrandUrl { get; set; }
 
+        /// <summary>
+        /// The URL, either relative or absolute, to an image that will be used with the brand. <c>.svg</c> images work best,
+        /// since they scale much better than bitmap based images such as <c>.png</c> or <c>.jpg</c>.
+        /// </summary>
         [Parameter]
         public string BrandImageUrl { get; set; }
 
+        /// <summary>
+        /// Allows you to freely customize the Brand element on the Navbar.
+        /// </summary>
         [Parameter]
         public RenderFragment<ILink> BrandTemplate { get; set; }
 
+        /// <summary>
+        /// Specifies whether the Brand is included in the collapsing part of the Navbar. The default, <c>false</c>, leaves the Brand element outside of the collapsing part of the Navbar.
+        /// </summary>
         [Parameter]
         public bool CollapseBrand { get; set; }
 
+        /// <summary>
+        /// Defines the colour scheme for your Navbar. Controls the colour of the text used in the Navbar. You also have to specify the background colour for the Navbar.
+        /// </summary>
         [Parameter]
         public NavbarColor? ColorScheme { get; set; }
 
+        /// <summary>
+        /// Specifies the responsive breakpoint at which the Navbar will expand from a collapsed state.
+        /// </summary>
         [Parameter]
         public NavbarExpandBreakpoint? ExpandAt { get; set; }
 
+        /// <summary>
+        /// Specifies the placement for the Navbar.
+        /// </summary>
         [Parameter]
         public NavbarPlacement? Placement { get; set; }
 
 
+        /// <summary>
+        /// </summary>
         protected override void OnParametersSet()
         {
             this.SetIdIfEmpty();
@@ -86,16 +119,40 @@ namespace Blazorade.Bootstrap.Components
 
     }
 
+    /// <summary>
+    /// Defines colour darkness for a <see cref="Navbar"/> component.
+    /// </summary>
     public enum NavbarColor
     {
+        /// <summary>
+        /// The <see cref="Navbar"/> background colour is a dark colour.
+        /// </summary>
         Dark,
+
+        /// <summary>
+        /// The <see cref="Navbar"/> background colour is a light colour.
+        /// </summary>
         Light
     }
 
+    /// <summary>
+    /// Defines different placements for a <see cref="Navbar"/> component.
+    /// </summary>
     public enum NavbarPlacement
     {
+        /// <summary>
+        /// The navbar is fixed to the top.
+        /// </summary>
         FixedTop,
+
+        /// <summary>
+        /// The navbar is fixed to the bottom.
+        /// </summary>
         FixedBottom,
+
+        /// <summary>
+        /// The navbar sticks to the top. Uses <c>position: sticky</c>, which may not be supported on all browsers.
+        /// </summary>
         StickyTop
     }
 }
