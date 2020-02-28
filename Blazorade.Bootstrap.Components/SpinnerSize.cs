9810599da87123db35b664e48dafdd005c4b331d
Mika Berglund <mika.berglund@meteoriitti.com>
2020-02-28 18:42:28 +0200

    #77

--- Blazorade.Bootstrap.Components/SpinnerSize.cs
+++ Blazorade.Bootstrap.Components/SpinnerSize.cs
@@ -4,9 +4,19 @@ using System.Text;
 
 namespace Blazorade.Bootstrap.Components
 {
+    /// <summary>
+    /// Defines different sizes for <see cref="Spinner"/> components.
+    /// </summary>
     public enum SpinnerSize
     {
+        /// <summary>
+        /// Normal size.
+        /// </summary>
         Normal,
+
+        /// <summary>
+        /// Small size. Useful for instance on buttons and links.
+        /// </summary>
         Small
     }
 }
