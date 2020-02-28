4ad163dca28645f4dda0611162402bc0314c0b66
Mika Berglund <mika.berglund@meteoriitti.com>
2020-02-28 18:53:58 +0200

    #82

--- Blazorade.Bootstrap.Components/ToggleAction.cs
+++ Blazorade.Bootstrap.Components/ToggleAction.cs
@@ -4,10 +4,24 @@ using System.Text;
 
 namespace Blazorade.Bootstrap.Components
 {
+    /// <summary>
+    /// Defines toggle actions for the <see cref="Modal"/> component.
+    /// </summary>
     public enum ToggleAction
     {
+        /// <summary>
+        /// Show the component.
+        /// </summary>
         Show,
+
+        /// <summary>
+        /// Hide the component.
+        /// </summary>
         Hide,
+
+        /// <summary>
+        /// Toggle the visibility of the component.
+        /// </summary>
         Toggle
     }
 }
