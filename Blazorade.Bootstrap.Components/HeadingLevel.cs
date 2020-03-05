b2a542e9ff04a88108ee325cabbb536b1c9e985a
Mika Berglund <mika.berglund@meteoriitti.com>
2020-03-05 14:42:12 +0200

    Changed HeadingLevel from enum to struct in order to be able to create implicit operators that we use to convert ints and strings to instances of HeadingLevel.
    With this change the markup that we have supported earlier does not change, but we can also do much simpler by just specifying  a number, because "1" = HeadingLevel.H1.
    
    The implicit operators are yet to be implemented.

--- Blazorade.Bootstrap.Components/HeadingLevel.cs
+++ Blazorade.Bootstrap.Components/HeadingLevel.cs
@@ -3,36 +3,80 @@
     /// <summary>
     /// Defines different levels for headings.
     /// </summary>
-    public enum HeadingLevel
+    public struct HeadingLevel
     {
+
+        internal const int H1Value = 1;
+
+        internal const int H2Value = 2;
+
+        internal const int H3Value = 3;
+
+        internal const int H4Value = 4;
+
+        internal const int H5Value = 5;
+
+        internal const int H6Value = 6;
+
+
+
+
         /// <summary>
         /// First level.
         /// </summary>
-        H1 = 1,
+        public static HeadingLevel H1
+        {
+            get { return new HeadingLevel { Value = H1Value }; }
+        }
 
         /// <summary>
         /// Second level.
         /// </summary>
-        H2 = 2,
+        public static HeadingLevel H2
+        {
+            get { return new HeadingLevel { Value = H2Value }; }
+        }
 
         /// <summary>
         /// Third level.
         /// </summary>
-        H3 = 3,
+        public static HeadingLevel H3
+        {
+            get { return new HeadingLevel { Value = H3Value }; }
+        }
 
         /// <summary>
         /// Fourth level.
         /// </summary>
-        H4 = 4,
+        public static HeadingLevel H4
+        {
+            get { return new HeadingLevel { Value = H4Value }; }
+        }
 
         /// <summary>
         /// Fifth level.
         /// </summary>
-        H5 = 5,
+        public static HeadingLevel H5
+        {
+            get { return new HeadingLevel { Value = H5Value }; }
+        }
 
         /// <summary>
         /// Sixth level.
         /// </summary>
-        H6 = 6
+        public static HeadingLevel H6
+        {
+            get { return new HeadingLevel { Value = H6Value }; }
+        }
+
+
+
+        private int _Value;
+        /// <summary>
+        /// The actual value of the 
+        /// </summary>
+        public int Value { get { return _Value; } private set { _Value = value; } }
+
     }
+
 }
