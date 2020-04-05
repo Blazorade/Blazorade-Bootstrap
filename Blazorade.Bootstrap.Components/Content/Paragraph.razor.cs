05d769ca79d1b19b89896fc5e86fdf672ecd7b10
Mika Berglund <mika.berglund@meteoriitti.com>
2020-04-05 14:44:00 +0300

    Added Paragraph.IsLead Completes #125

---
+++ Blazorade.Bootstrap.Components/Content/Paragraph.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazorade.Bootstrap.Components.Content
+{
+    partial class Paragraph
+    {
+
+        /// <summary>
+        /// Specifies whether the paragraph is a lead paragraph.
+        /// </summary>
+        [Parameter]
+        public bool IsLead { get; set; }
+
+        /// <summary>
+        /// </summary>
+        protected override void OnParametersSet()
+        {
+            if (this.IsLead)
+            {
+                this.AddClasses(ClassNames.Paragraphs.Lead);
+            }
+            base.OnParametersSet();
+        }
+    }
+}
