0a3de8257353dc98e012acc7436c9663426810ce
Mika Berglund <mika.berglund@meteoriitti.com>
2020-04-05 18:40:14 +0300

    Resolves #134.

---
+++ Blazorade.Bootstrap.Components/JumbotronContent.razor.cs
@@ -0,0 +1,24 @@
+﻿using Blazorade.Bootstrap.Components.Layout;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazorade.Bootstrap.Components
+{
+    partial class JumbotronContent
+    {
+        /// <summary>
+        /// </summary>
+        public JumbotronContent()
+        {
+            this.ContainerType = ContainerType.Fluid;
+        }
+
+        [Parameter]
+        public ContainerType ContainerType { get; set; }
+
+        [Parameter]
+        public bool IsFluid { get; set; }
+    }
+}
