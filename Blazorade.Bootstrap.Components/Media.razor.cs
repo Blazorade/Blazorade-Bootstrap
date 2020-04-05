b619eda218447aaa82a5f752873ec6e6c363a796
Mika Berglund <mika.berglund@meteoriitti.com>
2020-04-05 23:00:50 +0300

    Added few files required for the Media component. #135

---
+++ Blazorade.Bootstrap.Components/Media.razor.cs
@@ -0,0 +1,62 @@
+﻿using Blazorade.Bootstrap.Components.Content;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazorade.Bootstrap.Components
+{
+    partial class Media
+    {
+        /// <summary>
+        /// </summary>
+        public Media()
+        {
+            this.HeadingLevel = 5;
+            this.ImageHorizontalAlignment = HorizontalAlignment.Left;
+            this.ImageVerticalAlignment = VerticalAlignment.Top;
+        }
+
+        /// <summary>
+        /// The heading to display in the component.
+        /// </summary>
+        public string Heading { get; set; }
+
+        /// <summary>
+        /// The heading level. Defaults to 5.
+        /// </summary>
+        public HeadingLevel HeadingLevel { get; set; }
+
+        /// <summary>
+        /// The heading display size. Defaults to <c>null</c>.
+        /// </summary>
+        public HeadingDisplay? HeadingDisplay { get; set; }
+
+        /// <summary>
+        /// The URL to the image to show in the component.
+        /// </summary>
+        public string ImageUrl { get; set; }
+
+        /// <summary>
+        /// Allows you to completely control the rendering of the image.
+        /// </summary>
+        public RenderFragment<string> ImageTemplate { get; set; }
+
+        /// <summary>
+        /// The horizontal alignment of the image.
+        /// </summary>
+        public HorizontalAlignment ImageHorizontalAlignment { get; set; }
+
+        /// <summary>
+        /// The vertical alignment of the image.
+        /// </summary>
+        public VerticalAlignment ImageVerticalAlignment { get; set; }
+
+
+        protected override void OnParametersSet()
+        {
+
+            base.OnParametersSet();
+        }
+    }
+}
---
+++ Blazorade.Bootstrap.Components/VerticalAlignment.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazorade.Bootstrap.Components
+{
+    public struct VerticalAlignment
+    {
+
+        internal VerticalAlignment(string value)
+        {
+            this.Value = value;
+        }
+
+        public static implicit operator VerticalAlignment(string s)
+        {
+            return new VerticalAlignment(s);
+        }
+
+        public static bool operator ==(VerticalAlignment x, VerticalAlignment y)
+        {
+            return x.Value == y.Value;
+        }
+
+        public static bool operator !=(VerticalAlignment x, VerticalAlignment y)
+        {
+            return x.Value != y.Value;
+        }
+
+        internal const string TopValue = "top";
+        internal const string MiddleValue = "middle";
+        internal const string BottomValue = "bottom";
+
+
+        public static VerticalAlignment Top
+        {
+            get { return TopValue; }
+        }
+
+        public static VerticalAlignment Middle
+        {
+            get { return MiddleValue; }
+        }
+
+        public static VerticalAlignment Bottom
+        {
+            get { return BottomValue; }
+        }
+        
+
+        public string Value { get; set; }
+
+    }
+}
