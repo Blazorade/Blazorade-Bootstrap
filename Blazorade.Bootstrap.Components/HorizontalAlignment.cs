3aad0011000dbf3c37c7d66e214eda08266d1fca
Mika Berglund <mika.berglund@meteoriitti.com>
2020-05-05 13:26:56 +0300

    Changed HorizontalAlignment to enum.

--- Blazorade.Bootstrap.Components/HorizontalAlignment.cs
+++ Blazorade.Bootstrap.Components/HorizontalAlignment.cs
@@ -7,116 +7,22 @@ namespace Blazorade.Bootstrap.Components
     /// <summary>
     /// Defines different horizontal alignments values.
     /// </summary>
-    public struct HorizontalAlignment
+    public enum HorizontalAlignment
     {
-
-        internal const string LeftValue = "left";
-        internal const string CenterValue = "center";
-        internal const string RightValue = "right";
-
-        private static ICollection<string> Values = new List<string> { LeftValue, CenterValue, RightValue };
-
-        internal HorizontalAlignment(string value)
-        {
-            this.Value = value;
-        }
+        /// <summary>
+        /// Left alignment.
+        /// </summary>
+        Left,
 
         /// <summary>
-        /// Converts the given string to <see cref="HorizontalAlignment"/>.
+        /// Center alignment.
         /// </summary>
-        /// <param name="s"></param>
-        public static implicit operator HorizontalAlignment(string s)
-        {
-            if (Values.Contains(s?.ToLower()))
-            {
-                return new HorizontalAlignment(s?.ToLower());
-            }
-            return new HorizontalAlignment(null);
-        }
+        Center,
 
         /// <summary>
-        /// Eqals operator.
+        /// Right alignment.
         /// </summary>
-        public static bool operator ==(HorizontalAlignment x, HorizontalAlignment y)
-        {
-            return x.Value == y.Value;
-        }
-
-        /// <summary>
-        /// Not equals operator.
-        /// </summary>
-        public static bool operator !=(HorizontalAlignment x, HorizontalAlignment y)
-        {
-            return x.Value != y.Value;
-        }
-
-
-
-        /// <summary>
-        /// Content is aligned to the left.
-        /// </summary>
-        public static HorizontalAlignment Left
-        {
-            get { return new HorizontalAlignment(LeftValue); }
-        }
-
-        /// <summary>
-        /// Content is centered.
-        /// </summary>
-        public static HorizontalAlignment Center
-        {
-            get { return new HorizontalAlignment(CenterValue); }
-        }
-
-        /// <summary>
-        /// Content is aligned to the right.
-        /// </summary>
-        public static HorizontalAlignment Right
-        {
-            get { return new HorizontalAlignment(RightValue); }
-        }
-
-
-        /// <summary>
-        /// The actual value.
-        /// </summary>
-        public string Value { get; private set; }
-
-
-
-        /// <summary>
-        /// Compares the given object to the current instance.
-        /// </summary>
-        /// <param name="obj">The object to compare to the current.</param>
-        public override bool Equals(object obj)
-        {
-            if(obj is HorizontalAlignment)
-            {
-                HorizontalAlignment ca = (HorizontalAlignment)obj;
-                return this.Value == ca.Value;
-            }
-            return base.Equals(obj);
-        }
-
-        /// <summary>
-        /// Returns the hash code for the alignment value.
-        /// </summary>
-        /// <returns></returns>
-        public override int GetHashCode()
-        {
-            return this.Value?.GetHashCode() ?? 0;
-        }
-
-        /// <summary>
-        /// Returns the string representation.
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return this.Value;
-        }
-
+        Right
     }
-
 
 }
