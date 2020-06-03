7a592df0731a619c22bb6d406bc93ed02e2b12eb
Mika Berglund <mika.berglund@meteoriitti.com>
2020-06-03 15:55:27 +0300

    Docs improvement.

--- Blazorade.Bootstrap.Components/AlertLink.razor.cs
+++ Blazorade.Bootstrap.Components/AlertLink.razor.cs
@@ -7,6 +7,7 @@ namespace Blazorade.Bootstrap.Components
     /// <summary>
     /// This component can be used inside of <see cref="Alert"/> components to provide link colors matching with the parent <see cref="Alert"/> component.
     /// </summary>
+    /// <seealso cref="Alert"/>
     public partial class AlertLink
     {
         /// <summary>
