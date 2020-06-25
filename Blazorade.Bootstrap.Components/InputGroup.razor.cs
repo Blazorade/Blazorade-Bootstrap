6334e468d27d982cb74fafbf0086131b3a432552
Mika Berglund <mika.berglund@meteoriitti.com>
2020-06-25 07:39:21 +0300

    Added InputGroup component. WIP.

---
+++ Blazorade.Bootstrap.Components/InputGroup.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazorade.Bootstrap.Components
+{
+    /// <summary>
+    /// The <c>InputGroup</c> allows you to easily extend form controls by adding text, buttons, or button groups on either side of textual inputs, custom selects, and custom file inputs.
+    /// </summary>
+    partial class InputGroup
+    {
+
+        /// <summary>
+        /// The size of the input group.
+        /// </summary>
+        [Parameter]
+        public InputGroupSize Size { get; set; }
+
+    }
+}
---
+++ Blazorade.Bootstrap.Components/InputGroupSize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazorade.Bootstrap.Components
+{
+    public enum InputGroupSize
+    {
+        Small,
+        Default,
+        Large,
+
+    }
+}
